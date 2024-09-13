e9dc805a285c33eeba05c733b6dac1b17404f338
LinguiniConsumer <124742138+LinguiniConsumer@users.noreply.github.com>
2024-09-13 13:47:33 -0700

    Update pcbtable.cpp

--- pcbtable.cpp
+++ pcbtable.cpp
@@ -1,6 +1,6 @@
 /**
  * Assignment 1: priority queue of processes
- * @file pcbtable.h
+ * @file pcbtable.cpp
  * @author ??? (TODO: your name)
  * @brief This is the implementation file for the PCBTable class.
  * //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
